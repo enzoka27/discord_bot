7ae56dee42ea73cbc37eafb843d097eb6dd4f6e8
FahMenez <fa.meneses.jesus@gmail.com>
2026-03-16 20:22:48 -0300

    adicionado comando de ban

---
+++ src/commands/moderacao/ban.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Bane um usuário do servidor')
+        .addUserOption(opt =>
+          opt.setName('usuario')
+            .setDescription('Quem banir')
+            .setRequired(true))
+        .addStringOption(opt =>
+          opt.setName('motivo')
+            .setDescription('Motivo do ban'))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+    async execute(interaction){
+        if(!interaction.guild){
+            return interaction.reply({
+                content: 'Este comando só pode ser usado em servidores. Me adicione em um servidor e poderá utilizar os comando devidamente!',
+                ephemeral: true,
+            });
+        }
+
+        const usuarioAlvo = interaction.options.getUser('usuario');
+        const membro = await interaction.guild.members.fetch(usuarioAlvo.id).catch(() => null);
+        const motivo = interaction.options.getString('motivo') ?? 'Sem motivo';
+
+        try{
+            //bane o usuario do servidor
+            await interaction.guild.members.ban(usuarioAlvo, { reason: motivo });
+
+            await interaction.reply({
+                content: `${usuarioAlvo.username} foi banido. Motivo: ${motivo}`,
+                ephemeral: false,
+            });
+        } catch (erro){
+            await interaction.reply({
+                content: 'Não foi possível banir esse usuário. Verifique se o cargo do bot é superior ao cargo do usuário.',
+                ephemeral: false,
+            });
+        }
+    }
+}
