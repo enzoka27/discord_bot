97e2f11c67e7fdf145d2f070661aa71ce4da70b3
FahMenez <fa.meneses.jesus@gmail.com>
2026-03-16 01:22:25 -0300

    adicionado comando clear

---
+++ src/commands/moderacao/clear.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Apaga mensagens do canal')
+
+        //quantidade de mensagens a apagar(obrigatorio)
+        .addIntegerOption(opt => 
+          opt.setName('quantidade')
+            .setDescription('Quantas mensagens apagar(1-100)')
+            .setRequired(true)
+            .setMinValue(1)
+            .setMaxValue(100)) //o limite é 100 mensagens por vez (é possivel aumentar)    
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+    async execute(interaction){
+        if(!interaction.guild){
+            return interaction.reply({ content: 'Este comando só pode ser usado em servidores. Me adicione em um servidor e poderá utilizar os comando devidamente!'})
+        }
+
+        const quantidade = interaction.options.getInteger('quantidade');
+
+        try{
+            //apaga as mensagens do canal
+            const apagadas = await interaction.channel.bulkDelete(quantidade, true); //true = ignora mensagens com +14 dias
+
+            await interaction.reply({
+                content: `${apagadas.size} mensagens apagadas.`,
+                ephemeral: true,
+            });
+        } catch (erro){ //o dc n permite apagar msgs com mais de 14 dias via "bulkDelete", por isso o erro é tratado no catch
+            await interaction.reply({
+                content: 'Não foi possível apagar as mensagens. Verifique se as mensagens têm menos de 14 dias.',
+                ephemeral: true,
+            });
+        }
+    }
+};
