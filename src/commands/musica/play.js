65ff8f2ef29dab6c8e9139daa9b814aa12b87e9c
enzoka27 <enzobtsimoes@gmail.com>
2026-03-18 20:55:28 -0300

    ft: subida do inicio do comando play (musicas)

--- src/commands/musica/play.js
+++ src/commands/musica/play.js
@@ -1,5 +1,16 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {SlashCommandBuilder} = require('discord.js'); //importa o metodo slash... da biblioteca discordjs
 
-module.exports = {
-  data: teste
+module.exports = { //metodo embutido do nodejs que permite enviar dados pra outro arquivo com require
+  data: new SlashCommandBuilder() //utiliza o metodo slashcb e define suas configurações dentro do método
+        .setName('play')
+        .setDescription('Toca músicas por link ou nome'),
+        async execute(interaction) { 
+          if (!interaction.member.voice.channel){
+            //usuário não está em um canal de voz
+            await interaction.reply({content: 'Você não está em um canal de voz!', ephemeral: true});
+            //mensagem de erro
+            return; //finaliza a execução
+          }
+          const music = interaction.options.getString('busca');
+  }
 }
