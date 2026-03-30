396e19a7b814eca9060845b87514a246a162f07e
enzoka27 <enzobtsimoes@gmail.com>
2026-03-30 02:02:42 -0300

    interactionCreate mergeado corretamente

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,10 +1,7 @@
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
-<<<<<<< HEAD
-=======
     
->>>>>>> modulo-musicas
     if (!interaction.isChatInputCommand()) return;
 
     const comando = interaction.client.commands.get(interaction.commandName);
@@ -14,16 +11,12 @@ module.exports = {
       await comando.execute(interaction);
     } catch (erro) {
       console.error(erro);
-<<<<<<< HEAD
-      await interaction.reply({ content: '❌ Erro ao executar o comando.', ephemeral: true });
-=======
       if (interaction.deferred) {
         await interaction.editReply({ content: '❌ Erro ao executar o comando.' });
       } 
       else {
         await interaction.reply({ content: '❌ Erro ao executar o comando.', ephemeral: true });
 }
->>>>>>> modulo-musicas
     }
   }
 };
